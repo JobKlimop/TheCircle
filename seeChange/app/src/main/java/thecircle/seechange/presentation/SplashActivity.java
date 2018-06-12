5ccda843b81e12a8f9edfea7b12997387a44abd5
Whitney Cheung <whitneyrabbitc@gmail.com>
2018-06-12 20:23:52 +0200

    implemented the video streaming activity
    camera  and switching camera works, it will ask for a permission for the first time when starting the video streaming activity

---
+++ seeChange/app/src/main/java/thecircle/seechange/presentation/SplashActivity.java
@@ -0,0 +1,28 @@
+package thecircle.seechange.presentation;
+
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+import thecircle.seechange.R;
+
+public class SplashActivity extends AppCompatActivity {
+    protected int _splashTime = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,
+                        LoginActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 2000);
+    }
+}
